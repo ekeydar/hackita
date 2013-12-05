31544f9150a78ecc185334de48e41850c8a7a1ca
Eran Keydar <ekeydar@gmail.com>
2013-12-05 17:02:33 +0200

    wip

--- django_tut/mysite/polls/models.py
+++ django_tut/mysite/polls/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 import datetime
 
 class Poll(models.Model):
@@ -8,6 +9,9 @@ class Poll(models.Model):
         return self.question
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'    
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
